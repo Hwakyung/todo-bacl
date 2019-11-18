d949cbcc896318f1bb55c268bc086782da6dca8d
hwakyung <hwakyoung05@gmail.com>
2019-11-18 13:54:16 +0900

    login 구조 완성

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="login">
+  <h1>로그인페이지입니다.</h1>
+  <LoginForm/>
+</div>
+  
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm.vue'
+export default {
+  name:'Login',
+  components: {
+    
+    LoginForm
+  },
+}
+</script>
+
+<style>
+
+</style>
